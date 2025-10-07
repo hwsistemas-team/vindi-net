036224a0b0afef53c8d94ed56ecaa7c8440cec06
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2025-10-07 17:41:56 -0300

    Fix adrress zipcode fieldname

--- VindiSDK/Enttites/Address.cs
+++ VindiSDK/Enttites/Address.cs
@@ -7,7 +7,7 @@ namespace Vindi.SDK.Enttites
         public string Street { get; set; }
         public string Number { get; set; }
         public string AdditionalDetails { get; set; }
-        public string ZipCode { get; set; }
+        public string Zipcode { get; set; }
         public string Neighborhood { get; set; }
         public string City { get; set; }
         public string State { get; set; }
